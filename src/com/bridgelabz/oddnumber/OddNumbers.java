de23c6d9ae73682aaad26a9f1d7fac1b1501bf42
sahana199835 <tnsahana1@gmail.com>
2023-03-25 11:25:12 +0530

    add:check the sum of odd numbers

--- src/com/bridgelabz/oddnumber/OddNumbers.java
+++ src/com/bridgelabz/oddnumber/OddNumbers.java
@@ -4,7 +4,16 @@ public class OddNumbers {
 
 	public static void main(String[] args) {
 		
-		System.out.print("Welcome to odd numbers");
+		int N = 10, sum=0;
+		System.out.println("list the odd numbers");
+		
+		for (int i=1; i<=10;i++) {
+			if(i%2!=0) {
+				sum +=i;
+				System.out.println(i + " ");
+				System.out.println("Sum of all odd numbers between 1 to  " + N + " = " +sum);
+			}
+		}
 
 	}
 
