d737151a8bd8f664211117099aa3def497a5e724
sahana199835 <tnsahana1@gmail.com>
2023-03-25 11:07:26 +0530

    add:.gitignore and README.md files are added

---
+++ src/com/bridgelabz/oddnumber/OddNumbers.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.oddnumber;
+
+public class OddNumbers {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Welcome to odd numbers");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Pavan Kumar
+ *
+ */
+module WorkshopOne {
+}
