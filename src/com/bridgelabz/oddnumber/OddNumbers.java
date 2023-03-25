6c877f0ebb18bdca3d0a98f40f4ec4cca5859404
sahana199835 <tnsahana1@gmail.com>
2023-03-25 12:02:22 +0530

    add:adding array elements

--- src/com/bridgelabz/oddnumber/OddNumbers.java
+++ src/com/bridgelabz/oddnumber/OddNumbers.java
@@ -3,11 +3,15 @@ package com.bridgelabz.oddnumber;
 public class OddNumbers {
 
 	public static void main(String[] args) {
-		
+	   int i,j;
 		int N = 10, sum=0;
 		System.out.println("list the odd numbers");
 		
-		for (int i=1; i<=10;i++) {
+		for (int i1=1; i<=10;i++) {
+			n[i] = i + 10;
+			for (j=1;j<=10;j++) {
+				System.out.println("element stored at position: ");
+			}
 			if(i%2!=0) {
 				sum +=i;
 				System.out.println(i + " ");
